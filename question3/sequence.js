224215f0f3b8d28cb9e3e5d3c49823bb4f098a26
idem <victoridongesit@gmail.com>
2017-02-09 02:31:24 +0100

    Arithmetic and Geometric Arrays Algorithm

--- question3/sequence.js
+++ question3/sequence.js
@@ -1,5 +1,27 @@
 module.exports = {
- aritGeo: function() {
-   
+ aritGeo: function(data) {
+ 	var AP = true;
+ 	var GP = true;
+ 	if(data.length === 0 || data.length === 1){
+ 		return 0;
+ 	}
+
+ 	var commonDiff = data[0] - data[1];
+ 	for(var i = 0; i < data.length-1; i++){
+ 		var diff2 = data[i] - data[i+1];
+ 		if(commonDiff != diff2){
+ 			AP = false;
+ 			break;
+ 		}
+ 	}
+
+
+
+ 	if(AP){
+ 		return "Arithmetic";
+ 	}
+ 	if(AP){
+ 		return "Geometric";
+ 	}
  }
 }
