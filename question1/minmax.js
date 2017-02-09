4359113f7de3a8add385965ae7ec70de863ff8c9
idem <victoridongesit@gmail.com>
2017-02-09 01:56:25 +0100

    findMinMax Algorithm implemented correctly

--- question1/minmax.js
+++ question1/minmax.js
@@ -2,13 +2,19 @@
 
 module.exports = {
  findMinMax: function(data) {
+ 	var equal = true;
  	for(var i = 0; i < data.length -1; i++){
  		if(data[i] != data[i+1]){
+ 			equal = false;
  			break;
  		}
  	}
-   var max = Math.max.apply(null,data);
-   var min = Math.min.apply(null, data);
-   return [min, max];
+ 	if(!equal){
+	   var max = Math.max.apply(null,data);
+	   var min = Math.min.apply(null, data);
+	   return [min, max];
+	} else {
+		return [data[0]];
+	}
  }
 }
