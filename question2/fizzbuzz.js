065391dc10f4bd6f5e702365ddaa6095e4f53a04
idem <victoridongesit@gmail.com>
2017-02-09 02:06:08 +0100

    Added A FizzBuzz Algorithm

--- question2/fizzbuzz.js
+++ question2/fizzbuzz.js
@@ -1,5 +1,13 @@
 module.exports = {
- fizzBuzz: function() {
-   
+ fizzBuzz: function(number) {
+   if(number % 3 === 0 && number % 5!=0){
+   	return "Fizz";
+   } else if(number %5 ===0 && number %3 != 0){
+   	return "Buzz";
+   } else if (numbe %5 === 0 && number && 3 ===0){
+   	return "FizzBuzz";
+   } else{
+   	return number;
+   }
  }
 }
