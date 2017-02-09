4af69b1efcd99f0f4f49b6482c2bcc31de8f7dbb
idem <victoridongesit@gmail.com>
2017-02-09 01:51:34 +0100

    Algorithm for findMinMax in Progress

--- question1/minmax.js
+++ question1/minmax.js
@@ -2,6 +2,11 @@
 
 module.exports = {
  findMinMax: function(data) {
+ 	for(var i = 0; i < data.length -1; i++){
+ 		if(data[i] != data[i+1]){
+ 			break;
+ 		}
+ 	}
    var max = Math.max.apply(null,data);
    var min = Math.min.apply(null, data);
    return [min, max];
